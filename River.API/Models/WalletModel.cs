4b2450e3f941fe77c8650b49927d30c1a493cb75
kkopoku <kk.opoku@outlook.com>
2025-01-28 14:08:59 +0000

    chore: created wallet model

--- River.API/Models/WalletModel.cs
+++ River.API/Models/WalletModel.cs
@@ -4,20 +4,21 @@ using MongoDB.Bson.Serialization.Attributes;
 
 namespace River.API.Models;
 
-public class Wallet {
+public class Wallet
+{
 
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
-    
+    public string Id { get; set; }
+
+    // [BsonRepresentation(BsonType.ObjectId)]
+
+    // public required string CorridorId { get; set; }
+    // [BsonRepresentation(BsonType.ObjectId)]
+
+    // public required string OrganizationId { get; set; }
+
     public required string AccountNumber { get; set; }
-    [BsonRepresentation(BsonType.ObjectId)]
-
-    public required string CorridorId { get; set; }
-    [BsonRepresentation(BsonType.ObjectId)]
-
-    public required string Id { get; set; }
-
-    public required string OrganizationId { get; set; }
 
     public decimal Balance { get; set; }
 
@@ -25,12 +26,14 @@ public class Wallet {
 
     public required string LastName { get; set; }
 
-    public required string City { get; set; }
+    public string? Email { get; set; }
 
-    public required string PhoneNumber { get; set; }
+    public string? PhoneNumber { get; set; }
 
+    [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
     public DateTime CreatedAt { get; set; }
-    
+
+    [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
     public DateTime UpdatedAt { get; set; }
 
 }
