311bc6132adfa194850f4f9ca3fe86dbf50473f4
kkopoku <kk.opoku@outlook.com>
2025-01-28 14:07:49 +0000

    chore: created wallet controller

--- River.API/Controllers/WalletController.cs
+++ River.API/Controllers/WalletController.cs
@@ -1,23 +1,45 @@
 using Microsoft.AspNetCore.Mvc;
+using River.API.DTOs;
+using River.API.Services;
 
 namespace River.API.Controllers
 {
 
     [ApiController]
     [Route("api/[controller]")]
-
-    public class WalletController : ControllerBase
+    public class WalletController(
+        IWalletServices walletServices,
+        ILogger<WalletController> logger
+    ): ControllerBase
     {
 
+        private readonly IWalletServices _walletServices = walletServices;
+        private readonly ILogger<WalletController> _logger = logger;
+
         [HttpGet("test")]
-        public IActionResult TestRoute()
+        public IActionResult TestRoute([FromBody] object body)
         {
+            Console.WriteLine(body);
+
             var response = new {
                 message = "Hello World",
                 status = "OK"
             };
 
             return Ok(response);
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> CreateWallet([FromBody] CreateWalletDto createWalletDto){
+            string tag = "[WalletController][CreateWallet]";
+            _logger.LogInformation($"{tag} Wallet is being created via log");
+            try{
+                var createdWallet = await _walletServices.AddWalletAsync(createWalletDto);
+                return StatusCode(201);
+            }catch(Exception e){
+                Console.WriteLine(tag+e.Message);
+                return StatusCode(500);
+            }
         }
 
     }
