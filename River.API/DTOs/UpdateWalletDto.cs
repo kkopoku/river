734272d7a454a63c2197d04233684cc9e9e74236
kkopoku <kk.opoku@outlook.com>
2025-01-29 20:20:27 +0000

    chore: update wallet dto

---
+++ River.API/DTOs/UpdateWalletDto.cs
@@ -0,0 +1,15 @@
+
+
+namespace River.API.DTOs;
+
+
+public class UpdateWalletDto
+{
+    public required string AccountNumber { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Email { get; set; }
+    public string? PhoneNumber { get; set; }
+    public decimal? Balance { get; set; }
+
+}
